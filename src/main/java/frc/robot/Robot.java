745f500d3b6267f5c1384afbb8c9d5561a9eb841
dead-dad <59628520+dead-dad@users.noreply.github.com>
2020-01-16 17:47:07 -0800

    Testing Adding Weight Correction To Shooter
    Add weight correction to shooter. Work on equation for weight correction.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,13 +23,13 @@ public class Robot extends TimedRobot {
     bottomShooter = new SparkMax(1);
 
     //topShooter.setOutputRange(0.0, 0.25);
-    topShooter.setSpeedVariance(0.07);
-    topShooter.setP(0);
+    topShooter.setSpeedVariance(0.05);
+    topShooter.setP(0.0001);
     topShooter.setI(0);
     topShooter.setD(0);
 
     //bottomShooter.setOutputRange(0, 0);
-    bottomShooter.setSpeedVariance(0.07);
+    bottomShooter.setSpeedVariance(0.05);
     bottomShooter.setP(0.0001);
     bottomShooter.setI(0);
     bottomShooter.setD(0);
@@ -76,11 +76,19 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    double topShooterSpeed = 0.1;
-    double bottomShooterSpeed = -0.1;
+    double topShooterSpeed = 0.5;
+    double bottomShooterSpeed = -0.5;
 
-    topShooter.setSpeed(topShooterSpeed + (topShooterSpeed * 0.1338));
-    bottomShooter.setSpeed(bottomShooterSpeed + (bottomShooterSpeed * 0.1338));
+    double[] weightCorrection = new double[]{
+      ((37.7669895 * Math.pow(31.0431483, topShooterSpeed))/5700) + (100/5700),
+      ((37.7669895 * Math.pow(31.0431483, bottomShooterSpeed))/5700) + (100/5700)
+    };
+    //[0.1,0.15,0.2,0.25,0.3,0.35];
+    //[52.8573303,63.5714111,74.2857055,90.7139892,107.143578,123.571777];
+    // Equation is really cool look at this here bro ---> 37.7669895 * 31.0431483^weightCorrection
+ 
+    topShooter.setSpeed(topShooterSpeed);
+    bottomShooter.setSpeed(bottomShooterSpeed);
 
     //System.out.println(topShooter.getSpeed() * 5700);
     System.out.println(bottomShooter.getSpeed() * -5700);    
