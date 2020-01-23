16dbc62df4f105eb89f2125e581b9efe4c7fa502
dead-dad <59628520+dead-dad@users.noreply.github.com>
2020-01-23 14:11:59 -0800

    Speed Tuning For Test Shooter
    Speed test for the shooter prototype. Make graph for the speed of the shooter.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,8 +25,8 @@ public class Robot extends TimedRobot {
     top.setPID(1.2, 0.15, 0.15);
     bottom.setPID(1.2, 0.15, 0.15);
 
-    top.setFF(1.31147971);// 35 ft is 1.18447971
-    bottom.setFF(1.06429);// 35 ft is 1.10429
+    top.setFF(1.31147971);
+    bottom.setFF(1.06429);
 
     /*
     testFalcon.setP(1.2);
@@ -79,11 +79,16 @@ public class Robot extends TimedRobot {
     // For 10 feet: topshooter = 100% and bottom = 15%, angle = 60 degrees
     // For 44 feet: topshooter = 30% and bottom = 54%, angle = 60 degrees
 
-    // 35 feet: 21, 45 (top, bottom)
-    // 30 feet 13, 49 (top, bottom)
+    // 35 feet: 21, 45 (top, bottom) ff: 1.18447971, 1.10429
+    // 30 feet: 13, 49 (top, bottom)  ff: 1.31147971, 1.06429
+    // 25 feet: SAME AS 30 WHAT WHY PLEASE TELL ME
+    // 20 feet: 13, 43.5 (top, bottom) ff: 1.31147971, 1.06429
+    // 15 feet: 13, 47 (top, bottom) ff: 1.31147971, 1.06429
+    // 10 feet: 90, 13 (top, bottom) ff: 1.11147971, 1.16429
+    // 5 feet: NOT POSSIBLE DO NOT MAKE SPEED ABOVE 90 IT MIGHT DESTROY THE BALL
     
     top.setSpeed(0.13);
-    bottom.setSpeed(-0.49);
+    bottom.setSpeed(-0.375);
 
     System.out.println("top: " + top.getSpeed() * 100.0);
     System.out.println("bottom: " + bottom.getSpeed() * 100.0);
