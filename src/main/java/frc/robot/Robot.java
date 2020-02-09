fd9f6b77a18f8ad01824bfd75f45fb44d751b213
dead-dad <59628520+dead-dad@users.noreply.github.com>
2020-02-08 17:33:40 -0800

    Add Defualt auto path, add blank methods to robot.java
    See title

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,14 @@
 package frc.robot;
 
-import autonomous.*;
 import edu.wpi.first.wpilibj.TimedRobot;
 
-import wrappers.*;
-import networking.*;
+
+import autonomous.*;
 import hardware.*;
+import interfaces.*;
+import networking.*;
+import utils.*;
+import wrappers.*;
 
 public class Robot extends TimedRobot {
 
@@ -22,7 +25,7 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotInit() {
 
-    path = new AutoPath("/home/lvuser/deploy/autopaths/square.json");
+    path = new AutoPath("/home/lvuser/deploy/autopaths/Defualt.json");
 
     leftMotors = new PIDMotorGroup(new Falcon(1), new Falcon(2));
     rightMotors = new PIDMotorGroup(new Falcon(3), new Falcon(4));
@@ -50,6 +53,22 @@ public class Robot extends TimedRobot {
     auto.executeQueue();
 
   }
+
+  // NO TOUCH
+  @Override 
+  public void disabledInit() {}
+
+  // VERY NO TOUCH
+  @Override
+  public void disabledPeriodic() {
+
+    if(xbox.getButtonDown(XboxController.Buttons.A)) {
+
+      drive.bananaArcade(-xbox.getAxis(XboxController.Axes.LeftY), xbox.getAxis(XboxController.Axes.RightX));
+
+    }
+
+  }
   
 	@Override
 	public void teleopInit() {}
@@ -60,6 +79,9 @@ public class Robot extends TimedRobot {
     drive.bananaArcade(-xbox.getAxis(XboxController.Axes.LeftY), xbox.getAxis(XboxController.Axes.RightX));
 
   }
+  
+  @Override
+  public void testInit(){}
   
   @Override
   public void testPeriodic() {}
