55ee696b71bcd490ce93cbac86348f1a6fab2d40
dead-dad <59628520+dead-dad@users.noreply.github.com>
2021-01-30 19:22:31 -0800

    Ben broke it
    added negative

--- src/main/java/hardware/ShooterController.java
+++ src/main/java/hardware/ShooterController.java
@@ -100,7 +100,7 @@ public class ShooterController {
 
 		aligned = Math.abs(angle) <= acceptableAngleError;
 
-		double speed = -(aimLoop.getCommand(offset, angle) * converter); 
+		double speed = -(aimLoop.getCommand(-offset, angle) * converter); 
 
 		if(Math.abs(speed) < minSpeed) {
 
